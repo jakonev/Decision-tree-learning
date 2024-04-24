08be12a5482cc4923b031932186ac2e90bfc9c4d
jakonev <136200070+jakonev@users.noreply.github.com>
2024-04-24 15:17:47 -0300

    0002: Atualiza data.csv ano 22-04-2024. Explicacoes do codigo

--- excluir_previsao.py
+++ excluir_previsao.py
@@ -9,21 +9,21 @@ dados = pd.read_csv('data.csv')
 # Converter a coluna 'data_compra' para datetime
 dados['DATA_PREGAO'] = pd.to_datetime(dados['DATA_PREGAO'])
 
-# Media de precos calculados
+# Calcular o número de dias desde a data mais antiga até cada data de compra
 DIF_PRECO = dados['PRE-ABE']
-dados['DIF_COTACAO'] = (dados['PRE-ABE'] + dados['PRE-ULT'].min() + dados['PREMED']) / 3
+dados['DIF_COTACAO'] = (dados['PRE-ABE'].max() - dados['PRE-ULT'].min())
 
-# Codificar variáveis categóricas caso inclua codnego fracionado
+# Codificar variáveis categóricas
 label_encoder = LabelEncoder()
 dados['CODNEG'] = label_encoder.fit_transform(dados['CODNEG'])
 
-# Selecionar variáveis relevantes, opcional alterar a vontade
+# Selecionar variáveis relevantes
 variaveis = ['VOLT-TOTAL', 'PREMED', 'PRE-OFV', 'PRE-ULT', 'PRE-ABE']
 
 # Dividir os dados em conjunto de treinamento e teste
 X = dados[variaveis]
-y = dados['PRE-ULT']
-# X['PREMED'] = 3
+y = dados['PREMED']
+X['PREMED'] += dados['PRE-ULT'].min()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -37,11 +37,11 @@ modelo.fit(X_train, y_train)
 previsoes = modelo.predict(X_test)
 
 # Avaliar a precisão do modelo
+tabela = (X, previsoes)
+print(tabela)
 
-# Calcular a correlação entre o número de dias desde a data de referência e a variável
-correlacao_abertura = dados['PREMED'].corr(dados['PRE-ULT'])
-for i, predicao in enumerate(previsoes):
-    print(f"Resultado da previsão {i+1}: {predicao}")
+# Calcular a correlação entre o número de dias desde a data de referência e a variável de churn
+correlacao_abertura = dados['PREMED'].corr(dados['PRE-OFV'])
 
 # Visualizar a correlação
 print(f"Correlação PREMED e Oferta Venda: {correlacao_abertura:,.4f}")
